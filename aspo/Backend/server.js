6660563ee8d4ae67be8b8740791b190279435031
M-Zain-924823 <mz924823@gmail.com>
2026-02-13 21:40:32 +0500

    fixed cors url in backend

--- aspo/Backend/server.js
+++ aspo/Backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "https://plania-frontend.vercel.app"], 
+  origin: ["http://localhost:5173", "https://plania-aspo.vercel.app"], 
   credentials: true
 }));
 app.use(express.json());
